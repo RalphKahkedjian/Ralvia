c2c171cccd376a0919af2807916d93077c2aa540
ralphkian <ralph.kjian@hotmail.com>
2026-09-07 01:09:59 +0300

    Added differount route push after logging out

--- ralvia/src/components/LogoutButton.tsx
+++ ralvia/src/components/LogoutButton.tsx
@@ -10,7 +10,7 @@ export default function LogoutButton() {
     try {
       await logout();
 
-      router.push("/login");
+      router.push("/");
       router.refresh();
     } catch (error) {
       console.error(error);
